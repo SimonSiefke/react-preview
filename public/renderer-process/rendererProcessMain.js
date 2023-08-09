27d63c13e75f6fe1e9367874a10339f1f1c2c52b
Simon Siefke <simon.siefke@gmail.com>
2023-08-09 19:50:27 +0200

    add worker

--- public/renderer-process/rendererProcessMain.js
+++ public/renderer-process/rendererProcessMain.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const workerUrl = new URL(
+  '../renderer-worker/rendererWorkerMain.js',
+  import.meta.url,
+).toString()
+
+const worker = new Worker(workerUrl, {
+  type: 'module',
+  name: 'Renderer Worker',
+})
