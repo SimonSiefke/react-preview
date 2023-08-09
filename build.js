9f6bd6e4c21531cecc8c781e6ae6e662e123e301
Simon Siefke <simon.siefke@gmail.com>
2023-08-09 19:42:22 +0200

    copy files

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-import { mkdir, rm } from "node:fs/promises";
+import { cp, mkdir, rm } from "node:fs/promises";
 import path, { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -8,3 +8,5 @@ const root = path.join(__dirname, "..");
 const dist = join(root, "dist");
 await rm(dist, { recursive: true, force: true });
 await mkdir(dist, { recursive: true });
+
+await cp(join(root, "index.html"), join(dist, "index.html"));
