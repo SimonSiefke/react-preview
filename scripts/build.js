d2c5a8f92e1c5b0cc4fed86efb2799967fa5edeb
Simon Siefke <simon.siefke@gmail.com>
2023-08-09 19:43:45 +0200

    fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,10 +1,12 @@
-import { mkdir, rm } from "node:fs/promises";
-import path, { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
+import { cp, mkdir, rm } from 'node:fs/promises'
+import path, { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const root = path.join(__dirname, "..");
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = path.join(__dirname, '..')
 
-const dist = join(root, "dist");
-await rm(dist, { recursive: true, force: true });
-await mkdir(dist, { recursive: true });
+const dist = join(root, 'dist')
+await rm(dist, { recursive: true, force: true })
+await mkdir(dist, { recursive: true })
+
+await cp(join(root, 'index.html'), join(dist, 'index.html'))
