6093478452d0e0316632b94e0edb8c0e3809fd05
Simon Siefke <simon.siefke@gmail.com>
2023-08-09 19:39:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# React Preview
+
+> Simple preview for single file react components powered by WebWorkers.
+
+```sh
+git clone TODO &&
+cd TODO &&
+npm ci &&
+npm run dev
+```
---
+++ build.js
@@ -0,0 +1,10 @@
+import { mkdir, rm } from "node:fs/promises";
+import path, { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = path.join(__dirname, "..");
+
+const dist = join(root, "dist");
+await rm(dist, { recursive: true, force: true });
+await mkdir(dist, { recursive: true });
